d94f226eb6a9c2e3ea8af0ba61f46154b1c17be7
Mark Musa Mitrani <M.M.MitraniSabah@student.tudelft.nl>
2023-12-04 13:59:50 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+def main():
+    print("hi")
+    # load json, get input argument
+
+    # necessary structures:
+    ## args_player: list of player's arguments
+    ## args_computer: list of computer's arguments
+
+    # round loop: check if player can make attacks.
+    ## after player has made their move, check if their argument was used by computer before
+    ### if so, computer wins
+    ### if not, computer continues
+    ## check if computer can make an attack.
+    ## check if their argument was used by player before
+    ### if so, player wins
+    ### if not, player continues (next loop)
+    ## before start of next loop:
+
+if __name__ == '__main__':
+    main()
