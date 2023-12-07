69ccdb50ad52cad696330a94c223cdf318f10caa
ElifAyten <77199350+ElifAyten@users.noreply.github.com>
2023-12-07 16:03:43 +0100

    preferredextensions

--- preferred.py
+++ preferred.py
@@ -1,20 +1,16 @@
+# Check if a set is a subset of any other set
+def is_subset_of_any(set_, sets):
+    return any(set_ < other_set for other_set in sets)#set_ and a list of sets and return True if set_ is a proper subset of any set in sets
+
 def find_preferred_sets(admissible_sets):
     preferred_sets = []
-
-    # Check if a set is a subset of any other set
-    def is_subset_of_any(set_, sets):
-        return any(set_ < other_set for other_set in sets)#set_ and a list of sets and return True if set_ is a proper subset of any set in sets
-
     # Find preferred (we can also say max admissible) sets check each set
     for set_ in admissible_sets:
         if not is_subset_of_any(set_, admissible_sets):
             preferred_sets.append(set_)
-
     return preferred_sets
 
-# Example usage :)
-admissible_sets = [{"1","2"}, {"1", "4"}, {"1"}]  # Replace this with micheles new thing
+#ex usage 
+admissible_sets = [{"1", "2"}, {"1", "4"}, {"1"}]
 preferred_sets = find_preferred_sets(admissible_sets)
 print(preferred_sets)
-
-
