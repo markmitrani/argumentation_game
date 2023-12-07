f065fda23a291a0e47392f7e3b697b521c86fb12
Michele Vannucci <michele.vannucci20@gmail.com>
2023-12-07 16:11:30 +0100

    fixed adimissible

--- admissible.py
+++ admissible.py
@@ -1,33 +1,50 @@
-def find_admissible_sets(sets, info_graph):
-    admissible_sets = []
+ # Check if an element is defended
+def is_defended(element, set_, framework):
 
-    # Check if an element is defended
-    def is_defended(element, set_):
-        # An element defends itself if it attacks someone outside the set
-        if any(target not in set_ for target in info_graph[element]['attacks']):
-            return True
+    # get all attackers of the element
+    attackers = framework[element]['attacked']
 
-        # Or it is defended by another element in the set
-        for defender in set_:
-            if defender != element and element in info_graph[defender]['attacks']:
-                return True
-        return False
+    if len(attackers) == 0:
+        return True
+
+    for attacker in attackers:
+        # Check if the attacker is in the set
+        if attacker in set_:
+            assert False, "Internal attacks within the set"
+        
+        # Check if another node within the set attacks the attacker
+        if any(attacker in framework[element]['attacks'] for element in set_):
+            return True  
+
+    return False
+
+    # # An element defends itself if it attacks someone outside the set
+    # if any(target not in set_ for target in framework[element]['attacks']):
+    #     return True
+
+    # # Or it is defended by another element in the set
+    # for defender in set_:
+    #     if defender != element and element in framework[defender]['attacks']:
+    #         return True
+    # return False
+
+
+def find_admissible_sets(sets, framework):
+    admissible_sets = set()
 
     # Determine admissibility for each set
     for set_ in sets:
-        # Ensure no internal attacks within the set
-        if any(attacker in set_ for element in set_ for attacker in info_graph[element]['attacked']):
-            continue
 
         # Check if each element is defended
-        if all(is_defended(element, set_) for element in set_):
-            admissible_sets.append(set_)
+        if all(is_defended(element, set_, framework) for element in set_):
+            admissible_sets.add(set_)
 
     return admissible_sets
 
-# Example usage
-sets = [{"0", "2"}, {"0"},{"0","3"}]
-info_graph = {'0': {'attacks': ['1'], 'attacked': []}, '1': {'attacks': [], 'attacked': ['0', '2']}, '2': {'attacks': ['1', '3'], 'attacked': ['3']}, '3': {'attacks': ['2', '4'], 'attacked': ['2']}, '4': {'attacks': ['4'], 'attacked': ['3', '4']}}
-admissible_sets = find_admissible_sets(sets, info_graph)
-print(admissible_sets)
+if __name__ == '__main__':
+    # Example usage
+    sets = {frozenset({"0", "2"}), frozenset({"0"}),frozenset({"0","3"})}
+    framework = {'0': {'attacks': ['1'], 'attacked': []}, '1': {'attacks': [], 'attacked': ['0', '2']}, '2': {'attacks': ['1', '3'], 'attacked': ['3']}, '3': {'attacks': ['2', '4'], 'attacked': ['2']}, '4': {'attacks': ['4'], 'attacked': ['3', '4']}}
+    admissible_sets = find_admissible_sets(sets, framework)
+    print(admissible_sets)
 
---
+++ credulous_acceptance.py
@@ -0,0 +1,26 @@
+from admissible import find_admissible_sets
+from cf_single_argument import conflict_free_sets_containing_arg
+
+def credulous_acceptance(framework, argument):
+    pass # TODO
+
+
+if __name__=='__main__':
+    arg = '1'
+    framework = {
+        '0': {'attacks': ['1'], 'attacked': []},
+        '1': {'attacks': [], 'attacked': ['0', '2']},
+        '2': {'attacks': ['1', '3'], 'attacked': ['3']},
+        '3': {'attacks': ['2', '4'], 'attacked': ['2']},
+        '4': {'attacks': ['4'], 'attacked': ['3', '4']}
+    }
+    
+
+    print("-"*50)
+    print(f"Conflict-free sets containing the argument: {arg}")
+    cf_sets = conflict_free_sets_containing_arg(framework, arg)
+    print(cf_sets)
+    print("-"*50)
+    print(f"Admissible sets containing the argument: {arg}")
+    admissible_sets = find_admissible_sets(cf_sets, framework)
+    print(admissible_sets)
